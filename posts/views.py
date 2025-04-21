7bef0a2aea2d786974d7619f384e4c696d111a67
AnatoliyShevchenko <genitalgrinder90@outlook.com>
2025-04-21 21:08:05 +0500

    create post

--- posts/views.py
+++ posts/views.py
@@ -7,7 +7,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.db.utils import IntegrityError
 
-from posts.models import Posts, Images
+from posts.models import Posts, Images, Categories
 
 
 logger = logging.getLogger()
@@ -31,14 +31,35 @@ class PostsView(View):
 
     def get(self, request: HttpRequest) -> HttpResponse:
         is_active = request.user.is_active
+        categories = Categories.objects.all()
+        if not categories:
+            return HttpResponse(
+                content="<h1>Something went wrong</h1>"
+            )
         if not is_active:
             return redirect(to="login")
         return render(
-            request=request, template_name="post_form.html"
+            request=request, template_name="post_form.html",
+            context={"categories": categories}
         )
 
     def post(self, request: HttpRequest) -> HttpResponse:
-        pass
+        images = request.FILES.getlist("images")
+        post = Posts.objects.create(
+            user=request.user,
+            title=request.POST.get("title"),
+            description=request.POST.get("description")
+        )
+        post.categories.set(request.POST.getlist("categories"))
+        imgs = [Images(image=img, post=post) for img in images]
+        # for img in images:
+        #     Images.objects.create(
+        #         image=img,
+        #         post=post
+        #     )
+        Images.objects.bulk_create(imgs)
+        return redirect(to="base")
+        
 
     def put(self, request: HttpRequest) -> HttpResponse:
         pass
