53a5312229bf94c2eab2f6b15599f3faafa072ed
AnatoliyShevchenko <genitalgrinder90@outlook.com>
2025-04-21 21:17:51 +0500

    load static

--- settings/urls.py
+++ settings/urls.py
@@ -17,10 +17,21 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path(route="", view=include("clients.urls")),
     path(route="", view=include("posts.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        path('__debug__/', include('debug_toolbar.urls')),
+    ] + static(settings.MEDIA_URL, 
+               document_root=settings.MEDIA_ROOT
+    ) + static(settings.STATIC_URL, 
+               document_root=settings.STATIC_ROOT
+    )
+    
